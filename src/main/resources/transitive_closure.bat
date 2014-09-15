adb336e8135d9f3e1664fd8d2ea69cddb34c3657
bcarlsenca <bcarlsen@westcoastinformatics.com>
2014-09-15 11:49:48 -0700

    Get build pacakge ready for testing

---
+++ src/main/resources/transitive_closure.bat
@@ -0,0 +1,40 @@
+::
+:: Configurable parameters
+:: Please edit these variables to reflect your environment
+::
+set relsFile=c:/data/SNOMED/SnomedCT_Release_${editionLabel}_${editionVersion}/RF2Release/Snapshot/Terminology/sct2_Relationship_Snapshot_${editionLabel}_${editionVersion}.txt
+set outputFile = c:/data/SNOMED/SnomedCT_Release_${editionLabel}_${editionVersion}/RF2Release/Snapshot/Terminology/sct2_TransitiveClosure_Snapshot_${editionLabel}_${editionVersion}.txt
+
+del transitive_closure.log
+echo. > transitive_closure.log
+echo ---------------------------------------- >> transitive_closure.log 2>&1
+echo Starting ...  >> transitive_closure.log 2>&1
+date /T >> transitive_closure.log 2>&1
+time /T >> transitive_closure.log 2>&1
+echo ---------------------------------------- >> transitive_closure.log 2>&1
+echo relsFile = %relsFile% >> transitive_closure.log 2>&1
+echo outputFile = %outputFile% >> transitive_closure.log 2>&1
+set error=0
+
+:: NOTE: if this fails, make sure java is in the path
+::       or edit it to use the full path to java executable
+echo     Build transitive closure table >> transitive_closure.log 2>&1
+java -cp . com.wcinformatics.snomed.rf2.TransitiveClosureGenerator %relsFile% %outputFile%
+IF %ERRORLEVEL% NEQ 0 (set error=1
+goto trailer)
+
+:trailer
+echo ---------------------------------------- >> transitive_closure.log 2>&1
+IF %error% NEQ 0 (
+echo There were one or more errors. >> transitive_closure.log 2>&1
+set retval=-1
+) else (
+echo Completed without errors. >> transitive_closure.log 2>&1
+set retval=0
+)
+echo Finished ...  >> transitive_closure.log 2>&1
+date /T >> transitive_closure.log 2>&1
+time /T >> transitive_closure.log 2>&1
+echo ---------------------------------------- >> transitive_closure.log 2>&1
+pause
+exit %retval%
--- src/main/resources/transitive_closure.sh
+++ src/main/resources/transitive_closure.sh
@@ -1,166 +1,37 @@
 #!/bin/sh -f
-
 #
-# Database connection parameters
+# Configurable parameters
 # Please edit these variables to reflect your environment
 #
-ORACLE_HOME=/app/oracle/product/12.1.0/dbhome_1
-export ORACLE_HOME
-user=snomed
-password=snomed
-tns_name=global
-NLS_LANG=AMERICAN_AMERICA.UTF8
-export NLS_LANG
+relsFile=/data/SNOMED/SnomedCT_Release_${editionLabel}_${editionVersion}/RF2Release/Snapshot/Terminology/sct2_Relationship_Snapshot_${editionLabel}_${editionVersion}.txt
+outputFile = /data/SNOMED/SnomedCT_Release_${editionLabel}_${editionVersion}/RF2Release/Snapshot/Terminology/sct2_TransitiveClosure_Snapshot_${editionLabel}_${editionVersion}.txt
 
-/bin/rm -f oracle.log
-touch oracle.log
+/bin/rm -f transitive_closure.log
+touch transitive_closure.log
 ef=0
 
-echo "See oracle.log for output"
-echo "----------------------------------------" >> oracle.log 2>&1
-echo "Starting ... `/bin/date`" >> oracle.log 2>&1
-echo "----------------------------------------" >> oracle.log 2>&1
-echo "ORACLE_HOME = $ORACLE_HOME" >> oracle.log 2>&1
-echo "user =        $user" >> oracle.log 2>&1
-echo "tns_name =    $tns_name" >> oracle.log 2>&1
+echo "See transitive_closure.log for output"
+echo "----------------------------------------" >> transitive_closure.log 2>&1
+echo "Starting ... `/bin/date`" >> transitive_closure.log 2>&1
+echo "----------------------------------------" >> transitive_closure.log 2>&1
+echo "relsFile = $relsFile" >> transitive_closure.log 2>&1
+echo "outputFile = $outputFile" >> transitive_closure.log 2>&1
 
-echo "    Create tables ... `/bin/date`" >> oracle.log 2>&1
-echo "@oracle_tables.sql"|$ORACLE_HOME/bin/sqlplus $user/$password@$tns_name  >> oracle.log 2>&1
+# NOTE: if this fails, make sure java is in the path
+#       or edit it to use the full path to java executable
+echo "    Build transitive closure table ... `/bin/date`" >> transitive_closure.log 2>&1
+java -cp . com.wcinformatics.snomed.rf2.TransitiveClosureGenerator $relsFile $outputFile
 if [ $? -ne 0 ]; then ef=1; fi
- 
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="concept.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat concept.log >> oracle.log
-fi
 
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="description.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat description.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="identifier.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat identifier.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="relationship.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat relationship.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="statedrelationship.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat statedrelationship.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="textdefinition.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat textdefinition.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="associationreference.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat associationreference.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="attributevalue.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat attributevalue.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="simple.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat simple.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="complexmap.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat complexmap.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="extendedmap.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat extendedmap.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="simplemap.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat simplemap.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="language.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat language.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="refsetdescriptor.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat refsetdescriptor.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="descriptiontype.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat descriptiontype.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Load content table data ... `/bin/date`" >> oracle.log 2>&1
-$ORACLE_HOME/bin/sqlldr $user/$password@$tns_name control="moduledependency.ctl" >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-cat moduledependency.log >> oracle.log
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Create indexes ... `/bin/date`" >> oracle.log 2>&1
-echo "@oracle_indexes.sql"|$ORACLE_HOME/bin/sqlplus $user/$password@$tns_name  >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-fi
-
-if [ $ef -ne 1 ]; then
-echo "    Create views ... `/bin/date`" >> oracle.log 2>&1
-echo "@oracle_views.sql"|$ORACLE_HOME/bin/sqlplus $user/$password@$tns_name  >> oracle.log 2>&1
-if [ $? -ne 0 ]; then ef=1; fi
-fi
-
-echo "----------------------------------------" >> oracle.log 2>&1
+echo "----------------------------------------" >> transitive_closure.log 2>&1
 if [ $ef -eq 1 ]
 then
-  echo "There were one or more errors." >> oracle.log 2>&1
+  echo "There were one or more errors." >> transitive_closure.log 2>&1
   retval=-1
 else
-  echo "Completed without errors." >> oracle.log 2>&1
+  echo "Completed without errors." >> transitive_closure.log 2>&1
   retval=0
 fi
-echo "Finished ... `/bin/date`" >> oracle.log 2>&1
-echo "----------------------------------------" >> oracle.log 2>&1
+echo "Finished ... `/bin/date`" >> transitive_closure.log 2>&1
+echo "----------------------------------------" >> transitive_closure.log 2>&1
 exit $retval
